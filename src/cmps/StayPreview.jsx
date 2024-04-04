4aee1b59af8c2bed571e255933850243ecd28feb
OriChaiMatan <ori.chaimatan@gmail.com>
2024-04-04 21:02:41 +0300

    stay preview design

--- src/cmps/StayPreview.jsx
+++ src/cmps/StayPreview.jsx
@@ -1,13 +1,58 @@
 import { Link } from "react-router-dom";
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/scss/image-gallery.scss";
+
+
+import img_example1 from "../assets/img/stay_demo_img/2_3.png"
+import img_example2 from "../assets/img/stay_demo_img/2_2.png"
+import img_example3 from "../assets/img/stay_demo_img/3_2.png"
+import img_example4 from "../assets/img/stay_demo_img/2_1.png"
+import img_example5 from "../assets/img/stay_demo_img/1_1.png"
+
+const images = [
+    {
+        original: img_example1,
+        thumbnail: img_example1,
+    },
+    {
+        original: img_example2,
+        thumbnail: img_example2,
+    },
+    {
+        original: img_example3,
+        thumbnail: img_example3,
+    },
+    {
+        original: img_example4,
+        thumbnail: img_example4,
+    },
+    {
+        original: img_example5,
+        thumbnail: img_example5,
+    },
+]
 
 export function StayPreview({ stay }) {
+    // const images = stay.imgUrls.map(url => ({
+    //     original: url,
+    //     thumbnail: url 
+    // }));
+
     return (
         <div className="stay-preview">
+            <div className="stay-photo-gallery">
+                <ImageGallery
+                    items={images}
+                    showFullscreenButton={false}
+                    showPlayButton={false}
+                    showThumbnails={false}
+                    showBullets={true} />
+            </div>
             <Link to={`/stay/${stay._id}`}>
-                <div className="stay-preview-content">
-                    <img src={stay.imgUrls} alt="" />
-                    <span>{stay.name}</span>
-                    <span>${stay.price} per night</span>
+                <div className="stay-preview-information">
+                    <span className="name-info">{stay.name}</span>
+                    <span className="loc-info">{stay.loc.country}-{stay.loc.city}-{stay.loc.address}</span>
+                    <span className="price-info">${stay.price} <a>night</a></span>
                 </div>
             </Link>
         </div>
