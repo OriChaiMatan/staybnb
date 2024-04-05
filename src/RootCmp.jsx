86dc4cd43a56b1be44458a22e1eae353b84295ec
Irit-Holdarov <iritholdarov1233@gmail.com>
2024-04-05 22:18:26 +0300

    change RootCmp

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import { Route, HashRouter as Router, Routes } from 'react-router-dom';
-import { AppHeader } from './cmps/AppHeader'
-import { AppFooter } from './cmps/AppFooter'
-import { StayDetails } from './pages/StayDetails'
-import { StayIndex } from './pages/StayIndex'
-import { LabelsFilter } from './cmps/LabelsFilter';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { AppHeader } from './cmps/AppHeader';
+import { AppFooter } from './cmps/AppFooter';
+import { StayDetails } from './pages/StayDetails';
+import { StayIndex } from './pages/StayIndex';
 
 export function RootCmp() {
     return (
-        <main className='main-content'>
+        <div className='main-container'>
             <AppHeader />
-
             <main className='main-content'>
                 <Routes>
                     <Route path="/" element={<StayIndex />} />
+                    <Route path="/:filter" element={<StayIndex />} />
                     <Route path="/stay/:id" element={<StayDetails />} />
                 </Routes>
+            </main>
             <AppFooter />
-        </main>
+        </div>
     );
 }
 
