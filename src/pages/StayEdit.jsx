89a74fbfce87b26efd7cabc39295b3ae9fbb0994
OriChaiMatan <ori.chaimatan@gmail.com>
2024-05-26 16:44:49 +0300

    StayEdit- add more stay setting

--- src/pages/StayEdit.jsx
+++ src/pages/StayEdit.jsx
@@ -78,7 +78,7 @@ export function StayEdit(props) {
             <form className="stay-edit" onSubmit={onSaveStay}>
                 <section className="stay-title">
                     <input className="new-stay-name" type="text" name="name" placeholder="Stay name" value={stay.name} onChange={handleChange} />
-                   </section>
+                </section>
                 <section className="loc">
                     <input type="text" name="loc.country" placeholder="Country" value={stay.loc.country} onChange={handleChange} />
                     <input type="text" name="loc.city" placeholder="City" value={stay.loc.city} onChange={handleChange} />
@@ -92,8 +92,8 @@ export function StayEdit(props) {
                     <section className="add-buttom-right-img"><ImgUploader onUploaded={handleImgUpload} /></section>
                 </section>
                 <div className="type">
-                    <span>Capacity: <input type="number" name="capacity" value={stay.capacity} onChange={handleChange} /></span> •
-                    <span>StayType: <select name="type" value={stay.type} onChange={handleChange}>
+                    <span>Capacity: <input type="number" name="capacity" value={stay.capacity} onChange={handleChange} /></span>
+                    <span>Stay type: <select name="type" value={stay.type} onChange={handleChange}>
                         <option value="Entire Home/Apartment">Entire Home/Apartment</option>
                         <option value="Private Room">Private Room</option>
                         <option value="Shared Room">Shared Room</option>
@@ -101,28 +101,33 @@ export function StayEdit(props) {
                         <option value="Unique Stays">Unique Stays</option>
                         <option value="Camping">Camping</option>
                         <option value="RVs and Campervans">RVs and Campervans</option>
-                    </select></span> •
-                    <span>price: <input type="number" name="price" value={stay.price} onChange={handleChange} /> /night</span>
+                    </select></span>
+                    <span>price: <input type="number" name="price" value={stay.price} onChange={handleChange} /></span>
+                    <span>Beds: <input type="number" name="beds" value={stay.beds} onChange={handleChange} /></span>
+                    <span>Bedrooms: <input type="number" name="bedrooms" value={stay.bedrooms} onChange={handleChange} /></span>
+                    <span>Baths: <input type="number" name="bath" value={stay.bath} onChange={handleChange} /></span>
                 </div>
-                <div>
+                <section className="description">
                     <span>Description</span>
                     <textarea name="summary" id="" cols="50" rows="15" value={stay.summary} onChange={handleChange}></textarea>
-                </div>
+                </section>
                 <div className="amenities">
-                    <span>Amenities</span>
-                    <span><input type="checkbox" name="amenities" value="Wifi" onChange={handleChange} /> Wifi</span>
-                    <span><input type="checkbox" name="amenities" value="Kitchen" onChange={handleChange} /> Kitchen</span>
-                    <span><input type="checkbox" name="amenities" value="Washer" onChange={handleChange} /> Washer</span>
-                    <span><input type="checkbox" name="amenities" value="Dryer" onChange={handleChange} /> Dryer</span>
-                    <span><input type="checkbox" name="amenities" value="Air conditioning" onChange={handleChange} /> Air conditioning</span>
-                    <span><input type="checkbox" name="amenities" value="Heating" onChange={handleChange} /> Heating</span>
-                    <span><input type="checkbox" name="amenities" value="TV" onChange={handleChange} /> TV</span>
-                    <span><input type="checkbox" name="amenities" value="Iron" onChange={handleChange} /> Iron</span>
-                    <span><input type="checkbox" name="amenities" value="Pool" onChange={handleChange} /> Pool</span>
-                    <span><input type="checkbox" name="amenities" value="Pets allowed" onChange={handleChange} /> Pets allowed</span>
-                    <span><input type="checkbox" name="amenities" value="Gym" onChange={handleChange} /> Gym</span>
-                    <span><input type="checkbox" name="amenities" value="Smoking allowed" onChange={handleChange} /> Smoking allowed</span>
-                    <span><input type="checkbox" name="amenities" value="BBQ Grill" onChange={handleChange} /> BBQ Grill</span>
+                    <h1>Amenities</h1>
+                    <section className="amenities-list">
+                        <span><input type="checkbox" name="amenities" value="Wifi" onChange={handleChange} /> Wifi</span>
+                        <span><input type="checkbox" name="amenities" value="Kitchen" onChange={handleChange} /> Kitchen</span>
+                        <span><input type="checkbox" name="amenities" value="Washer" onChange={handleChange} /> Washer</span>
+                        <span><input type="checkbox" name="amenities" value="Dryer" onChange={handleChange} /> Dryer</span>
+                        <span><input type="checkbox" name="amenities" value="Air conditioning" onChange={handleChange} /> Air conditioning</span>
+                        <span><input type="checkbox" name="amenities" value="Heating" onChange={handleChange} /> Heating</span>
+                        <span><input type="checkbox" name="amenities" value="TV" onChange={handleChange} /> TV</span>
+                        <span><input type="checkbox" name="amenities" value="Iron" onChange={handleChange} /> Iron</span>
+                        <span><input type="checkbox" name="amenities" value="Pool" onChange={handleChange} /> Pool</span>
+                        <span><input type="checkbox" name="amenities" value="Pets allowed" onChange={handleChange} /> Pets allowed</span>
+                        <span><input type="checkbox" name="amenities" value="Gym" onChange={handleChange} /> Gym</span>
+                        <span><input type="checkbox" name="amenities" value="Smoking allowed" onChange={handleChange} /> Smoking allowed</span>
+                        <span><input type="checkbox" name="amenities" value="BBQ Grill" onChange={handleChange} /> BBQ Grill</span>
+                    </section>
 
 
                 </div>
