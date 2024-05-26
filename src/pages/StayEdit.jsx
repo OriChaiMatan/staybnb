8ac3339f8de5d3d68f95ccb26e43e6c011a96199
Irit-Holdarov <iritholdarov1233@gmail.com>
2024-05-26 15:11:44 +0300

    StayEdit grid settings

--- src/pages/StayEdit.jsx
+++ src/pages/StayEdit.jsx
@@ -74,56 +74,58 @@ export function StayEdit(props) {
     }
 
     return (
-        <form onSubmit={onSaveStay}>
-            <h1>Stay Edit</h1>
-            <input type="text" name="name" placeholder="stay name" value={stay.name} onChange={handleChange} />
-            <span><div className="star-icon"><Star /></div>New {stay.reviews.length} Reviews •</span>
-            <div className="loc">
-                <span> Country: <input type="text" name="loc.country" value={stay.loc.country} onChange={handleChange} /></span>
-                <span> City: <input type="text" name="loc.city" value={stay.loc.city} onChange={handleChange} /></span>
-                <span> Address: <input type="text" name="loc.address" value={stay.loc.address} onChange={handleChange} /></span>
-            </div>
-            <ImgUploader onUploaded={handleImgUpload} />
-            <ImgUploader onUploaded={handleImgUpload} />
-            <ImgUploader onUploaded={handleImgUpload} />
-            <ImgUploader onUploaded={handleImgUpload} />
-            <ImgUploader onUploaded={handleImgUpload} />
-            <div className="type">
-                <span>Capacity: <input type="number" name="capacity" value={stay.capacity} onChange={handleChange} /></span> •
-                <span>StayType: <select name="type" value={stay.type} onChange={handleChange}>
-                    <option value="Entire Home/Apartment">Entire Home/Apartment</option>
-                    <option value="Private Room">Private Room</option>
-                    <option value="Shared Room">Shared Room</option>
-                    <option value="Hotel">Hotel</option>
-                    <option value="Unique Stays">Unique Stays</option>
-                    <option value="Camping">Camping</option>
-                    <option value="RVs and Campervans">RVs and Campervans</option>
-                </select></span> •
-                <span>price: <input type="number" name="price" value={stay.price} onChange={handleChange} /> /night</span>
-            </div>
-            <div>
-                <span>Description</span>
-                <textarea name="summary" id="" cols="50" rows="15" value={stay.summary} onChange={handleChange}></textarea>
-            </div>
-            <div className="amenities">
-                <span>Amenities</span>
-                <span><input type="checkbox" name="amenities" value="Wifi" onChange={handleChange} /> Wifi</span>
-                <span><input type="checkbox" name="amenities" value="Kitchen" onChange={handleChange} /> Kitchen</span>
-                <span><input type="checkbox" name="amenities" value="Washer" onChange={handleChange} /> Washer</span>
-                <span><input type="checkbox" name="amenities" value="Dryer" onChange={handleChange} /> Dryer</span>
-                <span><input type="checkbox" name="amenities" value="Air conditioning" onChange={handleChange} /> Air conditioning</span>
-                <span><input type="checkbox" name="amenities" value="Heating" onChange={handleChange} /> Heating</span>
-                <span><input type="checkbox" name="amenities" value="TV" onChange={handleChange} /> TV</span>
-                <span><input type="checkbox" name="amenities" value="Iron" onChange={handleChange} /> Iron</span>
-                <span><input type="checkbox" name="amenities" value="Pool" onChange={handleChange} /> Pool</span>
-                <span><input type="checkbox" name="amenities" value="Pets allowed" onChange={handleChange} /> Pets allowed</span>
-                <span><input type="checkbox" name="amenities" value="Gym" onChange={handleChange} /> Gym</span>
-                <span><input type="checkbox" name="amenities" value="Smoking allowed" onChange={handleChange} /> Smoking allowed</span>
-                <span><input type="checkbox" name="amenities" value="BBQ Grill" onChange={handleChange} /> BBQ Grill</span>
+        <div className="stay-edit-container">
+            <form className="stay-edit" onSubmit={onSaveStay}>
+                <h1>Stay Edit</h1>
+                <input type="text" name="name" placeholder="stay name" value={stay.name} onChange={handleChange} />
+                <span><div className="star-icon"><Star /></div>New {stay.reviews.length} Reviews •</span>
+                <div className="loc">
+                    <span> Country: <input type="text" name="loc.country" value={stay.loc.country} onChange={handleChange} /></span>
+                    <span> City: <input type="text" name="loc.city" value={stay.loc.city} onChange={handleChange} /></span>
+                    <span> Address: <input type="text" name="loc.address" value={stay.loc.address} onChange={handleChange} /></span>
+                </div>
+                <ImgUploader onUploaded={handleImgUpload} />
+                <ImgUploader onUploaded={handleImgUpload} />
+                <ImgUploader onUploaded={handleImgUpload} />
+                <ImgUploader onUploaded={handleImgUpload} />
+                <ImgUploader onUploaded={handleImgUpload} />
+                <div className="type">
+                    <span>Capacity: <input type="number" name="capacity" value={stay.capacity} onChange={handleChange} /></span> •
+                    <span>StayType: <select name="type" value={stay.type} onChange={handleChange}>
+                        <option value="Entire Home/Apartment">Entire Home/Apartment</option>
+                        <option value="Private Room">Private Room</option>
+                        <option value="Shared Room">Shared Room</option>
+                        <option value="Hotel">Hotel</option>
+                        <option value="Unique Stays">Unique Stays</option>
+                        <option value="Camping">Camping</option>
+                        <option value="RVs and Campervans">RVs and Campervans</option>
+                    </select></span> •
+                    <span>price: <input type="number" name="price" value={stay.price} onChange={handleChange} /> /night</span>
+                </div>
+                <div>
+                    <span>Description</span>
+                    <textarea name="summary" id="" cols="50" rows="15" value={stay.summary} onChange={handleChange}></textarea>
+                </div>
+                <div className="amenities">
+                    <span>Amenities</span>
+                    <span><input type="checkbox" name="amenities" value="Wifi" onChange={handleChange} /> Wifi</span>
+                    <span><input type="checkbox" name="amenities" value="Kitchen" onChange={handleChange} /> Kitchen</span>
+                    <span><input type="checkbox" name="amenities" value="Washer" onChange={handleChange} /> Washer</span>
+                    <span><input type="checkbox" name="amenities" value="Dryer" onChange={handleChange} /> Dryer</span>
+                    <span><input type="checkbox" name="amenities" value="Air conditioning" onChange={handleChange} /> Air conditioning</span>
+                    <span><input type="checkbox" name="amenities" value="Heating" onChange={handleChange} /> Heating</span>
+                    <span><input type="checkbox" name="amenities" value="TV" onChange={handleChange} /> TV</span>
+                    <span><input type="checkbox" name="amenities" value="Iron" onChange={handleChange} /> Iron</span>
+                    <span><input type="checkbox" name="amenities" value="Pool" onChange={handleChange} /> Pool</span>
+                    <span><input type="checkbox" name="amenities" value="Pets allowed" onChange={handleChange} /> Pets allowed</span>
+                    <span><input type="checkbox" name="amenities" value="Gym" onChange={handleChange} /> Gym</span>
+                    <span><input type="checkbox" name="amenities" value="Smoking allowed" onChange={handleChange} /> Smoking allowed</span>
+                    <span><input type="checkbox" name="amenities" value="BBQ Grill" onChange={handleChange} /> BBQ Grill</span>
 
 
-            </div>
-            <button>Save</button>
-        </form>
+                </div>
+                <button>Save</button>
+            </form>
+        </div>
     )
 }
