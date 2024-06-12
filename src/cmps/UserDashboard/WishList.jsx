8dfce559b1eb5b1ce824f9e27a1cbf95bc210d14
taloreff <taloreff@gmail.com>
2024-06-12 22:59:06 +0300

    user wishlist real data

--- src/cmps/UserDashboard/WishList.jsx
+++ src/cmps/UserDashboard/WishList.jsx
@@ -4,32 +4,33 @@ import { useSelector } from "react-redux"
 
 import { StayList } from "../StayList"
 import { UserInfo } from "./UserProflie-cmps/UserInfo"
-import { loadStays } from "../../store/actions/stay.action"
+import { loadStays, saveStay } from "../../store/actions/stay.action"
 
 export function WishList() {
-  const demoLogInUser = {
-    id: "u110",
-    fullname: "Daniel Smith",
-    imgUrl: "http://res.cloudinary.com/dqti9icif/image/upload/v1716982768/men4_yio0gl.jpg",
-    username: "Daniel",
-    passworg: "daniel123",
-    address: "Kalisz, Poland"
-  }
 
+  const loggedInUser = useSelector((storeState) => storeState.userModule.user)
   const stays = useSelector((storeState) => storeState.stayModule.stays)
 
   useEffect(() => {
     loadStays()
   }, [])
 
+  async function onUpdateStay(stay) {
+    try {
+      await saveStay(stay)
+    } catch (err) {
+      console.log('Error in onUpdateStay', err)
+    }
+  }
+
   if (!stays) return (<div>loading....</div>)
 
-  const userLikedStays = stays.filter(stay => stay.likedByUsers.some(likedByUser => likedByUser.id === demoLogInUser.id))
+  const userLikedStays = stays.filter(stay => stay.likedByUsers.some(likedByUser => likedByUser._id === loggedInUser._id))
 
   return <div className="user-stay-list">
     <section className="user-list-title">
       <h1>My WishList</h1>
     </section>
-    <StayList stays={userLikedStays} />
+    <StayList stays={userLikedStays} onUpdateStay={onUpdateStay} />
   </div>
 }
